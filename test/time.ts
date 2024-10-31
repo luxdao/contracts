696b946bc01dae524ce8ff868c17ce90d2507e1f
Adam Gall <galladamj@gmail.com>
2024-10-31 17:22:01 -0400

    Add some more time helpers

--- test/time.ts
+++ test/time.ts
@@ -10,6 +10,16 @@ const advanceBlocks = async (blockCount: number) => {
   }
 };
 
+export const setTime = async (time: number) => {
+  await hre.ethers.provider.send('evm_setNextBlockTimestamp', [time]);
+  await hre.ethers.provider.send('evm_mine', []);
+};
+
+export const currentBlockTimestamp = async () => {
+  return (await hre.ethers.provider.getBlock(await hre.ethers.provider.getBlockNumber()))!
+    .timestamp;
+};
+
 const defaultExport = {
   advanceBlocks,
   advanceBlock,
