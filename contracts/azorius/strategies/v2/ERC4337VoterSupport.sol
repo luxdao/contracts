8bc389e3c754145ffde5083b3d9b30572a6e1047
Adam Gall <galladamj@gmail.com>
2025-02-19 11:37:41 -0500

    Fix _voter code, it didn't work when _msgSender was an EOA

--- contracts/azorius/strategies/v2/ERC4337VoterSupport.sol
+++ contracts/azorius/strategies/v2/ERC4337VoterSupport.sol
@@ -9,9 +9,23 @@ abstract contract ERC4337VoterSupport {
      * @param _msgSender address of the sender. It can be the wallet address, or the smart account address with EOA as owner
      * @return address of the voter
      */
-    function _voter(address _msgSender) internal virtual returns (address) {
+    function _voter(
+        address _msgSender
+    ) internal view virtual returns (address) {
+        // First check if the address has code (is a contract)
+        uint256 size;
+        assembly {
+            size := extcodesize(_msgSender)
+        }
+
+        // If it's an EOA (no code), return the address directly
+        if (size == 0) {
+            return _msgSender;
+        }
+
+        // If it's a contract, try to get its owner
         try IOwnership(_msgSender).owner() returns (address _value) {
-            return (_value);
+            return _value;
         } catch {
             return _msgSender;
         }
