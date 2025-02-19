b2474801f963bce83070f87b67bdbc85e60eedab
Adam Gall <galladamj@gmail.com>
2025-02-19 11:40:16 -0500

    Add mock contracts for testing

---
+++ contracts/mocks/MockSmartAccount.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {IOwnership} from "../interfaces/IOwnership.sol";
+
+contract MockSmartAccount is IOwnership {
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+}
