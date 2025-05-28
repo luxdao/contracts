b067ae6b03d6de74b0846d877b1e2290f39b73cd
Taylor Webb <tbwebb22@gmail.com>
2025-05-28 09:30:40 -0600

    base staking contract init commit

---
+++ contracts/deployables/staking/VotesStakedERC20V1.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.30;
+
+import {Version} from "../Version.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {NoncesUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
+import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+/**
+ * A staking contract that immediately distributes rewards in full
+ * to all stakers upon each distribution.
+ */
+contract VotesStakedERC20V1 is
+    Version,
+    ERC20VotesUpgradeable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
+    uint16 private constant VERSION = 1;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * Initialize function, will be triggered when a new proxy instance is deployed.
+     *
+     * @param name Token name
+     * @param symbol Token symbol
+     * @param owner Address that will own the proxy and be able to upgrade it
+     */
+    function initialize(
+        string memory name,
+        string memory symbol,
+        address owner
+    ) public initializer {
+        __ERC20_init(name, symbol);
+        __ERC20Votes_init();
+        __UUPSUpgradeable_init();
+        __Ownable_init(owner);
+    }
+
+    function clock() public view override returns (uint48) {
+        return uint48(block.timestamp);
+    }
+
+    function CLOCK_MODE() public pure override returns (string memory) {
+        return "mode=timestamp";
+    }
+
+    /**
+     * @dev Function that should revert when `msg.sender` is not authorized to upgrade the contract.
+     * Called by {upgradeTo} and {upgradeToAndCall}.
+     *
+     * Reverts if the sender is not the owner of the contract.
+     */
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override onlyOwner {
+        // Authorization is handled by the onlyOwner modifier
+    }
+
+    /// @inheritdoc Version
+    function getVersion() public view virtual override returns (uint16) {
+        return VERSION;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IERC20).interfaceId ||
+            interfaceId == type(IVotes).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
