aad900765c849b3c05136fdd412cae4307ed5642
twodam <twodam@protonmail.com>
2025-05-28 15:46:30 +0800

    feat(VotesERCLockableV1): add maxTotalSupply variable and setter function which limits the amount that mint function can use

--- contracts/interfaces/decent/deployables/IMintableV1.sol
+++ contracts/interfaces/decent/deployables/IMintableV1.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.30;
 
 interface IMintableV1 {
+    event MaxTotalSupplyUpdated(uint256 newMaxTotalSupply);
+
+    error ExceedMaxTotalSupply();
+
+    function maxTotalSupply() external view returns (uint256);
+
     function mint(address to, uint256 amount) external;
 }
