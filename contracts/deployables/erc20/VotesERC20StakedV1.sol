b274191c0481c28ed0e6e97b8e91e4cea048da08
Taylor Webb <tbwebb22@gmail.com>
2025-05-29 16:30:47 -0600

    create IVotesERC20Staked interface

--- contracts/deployables/erc20/VotesERC20StakedV1.sol
+++ contracts/deployables/erc20/VotesERC20StakedV1.sol
@@ -4,25 +4,21 @@ pragma solidity ^0.8.30;
 import {Version} from "../Version.sol";
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
-import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import {ERC20VotesUpgradeable, VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import {IVotesERC20StakedV1} from "../../interfaces/decent/deployables/IVotesERC20StakedV1.sol";
 
 contract VotesERC20StakedV1 is
+    IVotesERC20StakedV1,
     Version,
     ERC20VotesUpgradeable,
     UUPSUpgradeable,
     Ownable2StepUpgradeable
 {
     uint16 private constant VERSION = 1;
-    IERC20 public stakedToken;
-    uint256 public minimumStakingPeriod;
-
-    event MinimumStakingPeriodUpdated(uint256 newMinimumStakingPeriod);
-
-    error NonTransferable();
+    IERC20 internal _stakedToken;
+    uint256 internal _minimumStakingPeriod;
 
     constructor() {
         _disableInitializers();
@@ -32,20 +28,20 @@ contract VotesERC20StakedV1 is
         string memory name,
         string memory symbol,
         address owner,
-        address _stakedToken,
-        uint256 _minimumStakingPeriod
-    ) public initializer {
+        address stakedToken_,
+        uint256 minimumStakingPeriod_
+    ) public virtual override initializer {
         __ERC20_init(name, symbol);
         __ERC20Votes_init();
         __UUPSUpgradeable_init();
         __Ownable_init(owner);
-        stakedToken = IERC20(_stakedToken);
-        _updateMinimumStakingPeriod(_minimumStakingPeriod);
+        _stakedToken = IERC20(stakedToken_);
+        _updateMinimumStakingPeriod(minimumStakingPeriod_);
     }
 
     function updateMinimumStakingPeriod(
         uint256 newMinimumStakingPeriod
-    ) public onlyOwner {
+    ) external virtual override onlyOwner {
         _updateMinimumStakingPeriod(newMinimumStakingPeriod);
     }
 
@@ -53,19 +49,34 @@ contract VotesERC20StakedV1 is
         revert NonTransferable();
     }
 
-    function transferFrom(address, address, uint256) public virtual override returns (bool) {
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) public virtual override returns (bool) {
         revert NonTransferable();
-    }   
+    }
 
     function approve(address, uint256) public virtual override returns (bool) {
         revert NonTransferable();
     }
 
-    function clock() public view override returns (uint48) {
+    function clock()
+        public
+        view
+        virtual
+        override(IVotesERC20StakedV1, VotesUpgradeable)
+        returns (uint48)
+    {
         return uint48(block.timestamp);
     }
 
-    function CLOCK_MODE() public pure override returns (string memory) {
+    function CLOCK_MODE()
+        public
+        pure
+        override(IVotesERC20StakedV1, VotesUpgradeable)
+        returns (string memory)
+    {
         return "mode=timestamp";
     }
 
@@ -78,7 +89,7 @@ contract VotesERC20StakedV1 is
     function _updateMinimumStakingPeriod(
         uint256 newMinimumStakingPeriod
     ) internal {
-        minimumStakingPeriod = newMinimumStakingPeriod;
+        _minimumStakingPeriod = newMinimumStakingPeriod;
         emit MinimumStakingPeriodUpdated(newMinimumStakingPeriod);
     }
 
@@ -86,10 +97,25 @@ contract VotesERC20StakedV1 is
         return VERSION;
     }
 
+    function stakedToken() external view virtual override returns (address) {
+        return address(_stakedToken);
+    }
+
+    function minimumStakingPeriod()
+        external
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return _minimumStakingPeriod;
+    }
+
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override returns (bool) {
         return
+            interfaceId == type(IVotesERC20StakedV1).interfaceId ||
             interfaceId == type(IERC20).interfaceId ||
             interfaceId == type(IVotes).interfaceId ||
             super.supportsInterface(interfaceId);
