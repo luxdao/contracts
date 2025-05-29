9f4ed8ffac74a5b9e4bc7a0be3245b7027a7bae6
Taylor Webb <tbwebb22@gmail.com>
2025-05-29 16:08:51 -0600

    remove natspec

--- contracts/deployables/erc20/VotesERC20StakedV1.sol
+++ contracts/deployables/erc20/VotesERC20StakedV1.sol
@@ -10,10 +10,6 @@ import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/E
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
-/**
- * Staking contract that immediately distributes rewards in full
- * to all stakers upon each distribution.
- */
 contract VotesERC20StakedV1 is
     Version,
     ERC20VotesUpgradeable,
@@ -32,15 +28,6 @@ contract VotesERC20StakedV1 is
         _disableInitializers();
     }
 
-    /**
-     * Initialize function, will be triggered when a new proxy instance is deployed.
-     *
-     * @param name Token name
-     * @param symbol Token symbol
-     * @param owner Address that will own the proxy and be able to upgrade it
-     * @param _stakedToken Address of the token to be staked
-     * @param _minimumStakingPeriod Minimum staking period in seconds
-     */
     function initialize(
         string memory name,
         string memory symbol,
@@ -56,26 +43,16 @@ contract VotesERC20StakedV1 is
         _updateMinimumStakingPeriod(_minimumStakingPeriod);
     }
 
-    /**
-     * @notice Updates the minimum staking period, only callable by the owner.
-     * @param newMinimumStakingPeriod The new minimum staking period in seconds.
-     */
     function updateMinimumStakingPeriod(
         uint256 newMinimumStakingPeriod
     ) public onlyOwner {
         _updateMinimumStakingPeriod(newMinimumStakingPeriod);
     }
 
-    /**
-     * @notice This ERC20 function is overridden to prevent transfers
-     */
     function transfer(address, uint256) public virtual override returns (bool) {
         revert NonTransferable();
     }
 
-    /**
-     * @notice This ERC20 function is overridden to prevent transferFroms
-     */
     function transferFrom(address, address, uint256) public virtual override returns (bool) {
         revert NonTransferable();
     }   
@@ -92,22 +69,12 @@ contract VotesERC20StakedV1 is
         return "mode=timestamp";
     }
 
-    /**
-     * @dev Function that should revert when `msg.sender` is not authorized to upgrade the contract.
-     * Called by {upgradeTo} and {upgradeToAndCall}.
-     *
-     * Reverts if the sender is not the owner of the contract.
-     */
     function _authorizeUpgrade(
         address newImplementation
     ) internal virtual override onlyOwner {
         // Authorization is handled by the onlyOwner modifier
     }
 
-    /**
-     * @notice Updates the minimum staking period.
-     * @param newMinimumStakingPeriod The new minimum staking period in seconds.
-     */
     function _updateMinimumStakingPeriod(
         uint256 newMinimumStakingPeriod
     ) internal {
@@ -115,7 +82,6 @@ contract VotesERC20StakedV1 is
         emit MinimumStakingPeriodUpdated(newMinimumStakingPeriod);
     }
 
-    /// @inheritdoc Version
     function getVersion() public view virtual override returns (uint16) {
         return VERSION;
     }
