a588f1c17c97dfe9fa6b8d90b3bddcad534fab48
Taylor Webb <tbwebb22@gmail.com>
2025-06-12 10:41:07 -0600

    create MockKYCVerifier contract

---
+++ contracts/mocks/MockKYCVerifier.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.30;
+
+import {IKYCVerifierV1} from "../interfaces/decent/deployables/IKYCVerifierV1.sol";
+import {IVersion} from "../interfaces/decent/deployables/IVersion.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+contract MockKYCVerifier is IKYCVerifierV1, IVersion, ERC165 {
+    // ======================================================================
+    // STATE VARIABLES
+    // ======================================================================
+
+    bool internal _verify;
+    address internal _verifier;
+
+    constructor(address verifier_) {}
+
+    function initialize(
+        address verifier_,
+        string memory,
+        string memory
+    ) public {
+        _verifier = verifier_;
+    }
+
+    function setVerify(bool verify_) public {
+        _verify = verify_;
+    }
+
+    function verify(
+        SignData memory,
+        bytes memory
+    ) public view virtual override returns (bool) {
+        return _verify;
+    }
+
+    function verifier() public view virtual override returns (address) {
+        return _verifier;
+    }
+
+    function version() public pure virtual override returns (uint16) {
+        return 1;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId_
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId_ == type(IKYCVerifierV1).interfaceId ||
+            interfaceId_ == type(IVersion).interfaceId ||
+            super.supportsInterface(interfaceId_);
+    }
+}
