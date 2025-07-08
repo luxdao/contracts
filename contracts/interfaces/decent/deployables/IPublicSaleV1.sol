13857e1186651faa06d62d9bcfb508cbad209c79
Taylor Webb <tbwebb22@gmail.com>
2025-07-08 14:35:36 -0600

    commit function revert checks

--- contracts/interfaces/decent/deployables/IPublicSaleV1.sol
+++ contracts/interfaces/decent/deployables/IPublicSaleV1.sol
@@ -4,28 +4,57 @@ pragma solidity ^0.8.30;
 interface IPublicSaleV1 {
     // --- Errors ---
 
-    error InvalidSaleCloseTimestamp();
+    error InvalidSaleEndTimestamp();
 
-    error InvalidSaleTokenAmounts();
+    error InvalidSaleTimestamps();
+
+    error InvalidCommitmentAmounts();
+
+    error TransferFailed();
+
+    error InvalidBidAmount();
+
+    error SaleCloseTimestampNotElapsed();
+
+    error SaleNotStarted();
+
+    error SaleClosed();
+
+    error MinimumCommitment();
+
+    error MaximumCommitment();
+
+    error MaximumTotalCommitment();
 
     // --- Structs ---
 
+    struct InitializerParams {
+        address owner_;
+        address commitmentToken_;
+        address saleToken_;
+        uint256 minimumCommitment_;
+        uint256 maximumCommitment_;
+        uint256 minimumTotalCommitment_;
+        uint256 maximumTotalCommitment_;
+        uint256 saleTokenPrice_;
+        uint48 saleStartTimestamp_;
+        uint48 saleCloseTimestamp_;
+        address saleTokenHolder_;
+    }
+
     // --- Events ---
+
+    event BidPlaced(address indexed user, uint256 amount);
 
     // --- Initializer Functions ---
 
-    function initialize(
-        address owner_,
-        address feeToken_,
-        address saleToken_,
-        uint256 saleTokenMinimumAmount_,
-        uint256 saleTokenMaximumAmount_,
-        uint256 saleTokenPrice_,
-        uint48 saleStartTimestamp_,
-        uint48 saleCloseTimestamp_
-    ) external;
+    function initialize(InitializerParams memory params_) external;
 
     // --- View Functions ---
 
     // --- State-Changing Functions ---
+
+    function commit(uint256 amount_) external payable;
+
+    function close() external;
 }
